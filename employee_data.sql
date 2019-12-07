85b2fee90ec2b7ebb5b37f8d27f42336ae0a6ad5
Pronojit Mallick Jit <mallick.pronojit@gmail.com>
2019-12-07 15:14:26 +0600

    Delete employee_data.sql

--- employee_data.sql
+++
@@ -1,5 +0,0 @@
-create table employee_data(
-E_ID INTEGER NOT NULL PRIMARY KEY, Name VARCHAR (20), Designation VARCHAR (15),
- Gender VARCHAR (25), Age INTEGER,Contact_No VARCHAR (15), Joining_Date VARCHAR(15), Leaving_Date VARCHAR(15));
-
- select * from employee_data
